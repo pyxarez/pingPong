b2703d3c62b46c4fb3dd3d0bc23caf0e9b8d7726
pyxarez <pyxarez@gmail.com>
2017-09-22 00:05:10 +0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+console.log('biba');
+
+const canvas = document.createElement('canvas');
+canvas.classList.add('canvas');
+document.body.append(canvas);
+
+const ctx = canvas.getContext('2d');
